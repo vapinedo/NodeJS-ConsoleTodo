59d21f30db44327d5faf9802fdfd23340f8f9771
Victor Pinedo <victor.piendo@masglobalconsulting.com>
2022-06-13 08:34:02 -0500

    [add] 51. Logica para el manejo de las tareas por hacer

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 require("colors");
 const { inquirerMenu, pausa } = require("./helpers/inquirer");
-
-console.clear();
+const Tarea = require("./models/tarea");
+const Tareas = require("./models/tareas");
 
 const main = async () => {
   console.log("Hello world");
 
   let opt = null;
   do {
-    opt = await inquirerMenu();
-    console.log({ opt });
+    // opt = await inquirerMenu();
+    // console.log({ opt });
+    const tareas = new Tareas();
+    const tarea = new Tarea("Comprar comida");
+    tareas._listado[tarea.id] = tarea;
+    console.log(tareas);
+
     await pausa();
   } while (opt !== 0);
 };
